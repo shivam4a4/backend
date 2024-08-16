845882b076e8d338ff48e2c29a898af712cf0d7a
Shivam4a4 <negishivam9389@gmail.com>
2024-08-16 13:30:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req,res)=>{
+    res.send('shivamdotcom')
+})
+app.get('/login', (req,res)=>{
+    res.send('<h1>please login</h1>')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
